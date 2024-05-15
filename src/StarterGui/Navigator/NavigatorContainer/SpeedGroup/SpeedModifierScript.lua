a04cbad19acca4fb60618e3bfe7f937af277b325
Christian Toney <christian@makuwro.com>
2024-05-14 23:53:06 -0400

    Create SpeedModifierScript.lua

---
+++ src/StarterGui/Navigator/NavigatorContainer/SpeedGroup/SpeedModifierScript.lua
@@ -0,0 +1,45 @@
+--!strict
+-- SpeedModifierScript.lua
+-- Written by Christian "Sudobeast" Toney 
+local defaultSpeed = 16;
+local player = game:GetService("Players").LocalPlayer;
+local currentSpeedTextBox = script.Parent.CanvasGroup.CurrentSpeed;
+local defaultSpeedButton = script.Parent.CanvasGroup.Default;
+function changeDefaultSpeed(newSpeed: number): ()
+  
+  defaultSpeed = newSpeed;
+  defaultSpeedButton.Visible = defaultSpeed ~= tonumber(currentSpeedTextBox.PlaceholderText);
+  
+end;
+
+if player.Character then
+  
+  local humanoid = player.Character.Humanoid;
+  currentSpeedTextBox.PlaceholderText = humanoid.WalkSpeed;
+  changeDefaultSpeed(humanoid.WalkSpeed);
+  humanoid:GetPropertyChangedSignal("WalkSpeed"):Connect(function()
+    
+    currentSpeedTextBox.PlaceholderText = humanoid.WalkSpeed;
+    defaultSpeedButton.Visible = defaultSpeed ~= tonumber(currentSpeedTextBox.PlaceholderText);
+    
+  end)
+  
+end
+  
+currentSpeedTextBox.FocusLost:Connect(function(enterPressed)
+  
+  if enterPressed then
+    
+    player.Character.Humanoid.WalkSpeed = tonumber(currentSpeedTextBox.Text);
+    
+  end
+    
+  currentSpeedTextBox.Text = "";
+  
+end)
+
+defaultSpeedButton.MouseButton1Click:Connect(function() 
+  
+  player.Character.Humanoid.WalkSpeed = defaultSpeed;
+  
+end)
