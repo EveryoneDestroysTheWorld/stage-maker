42c0448913564a9b9bb689439c7b77b7dc3440c1
Christian Toney <christian@makuwro.com>
2024-05-14 23:48:32 -0400

    Create ServerScriptService.lua

---
+++ src/ServerScriptService.lua
@@ -0,0 +1,15 @@
+--!strict
+game:GetService("ReplicatedStorage").TogglePlayerCollision.OnServerInvoke = function(player, mode)
+
+  -- Let the player go through walls.
+  for _, instance in ipairs(player.Character:GetChildren()) do
+    
+    if instance:IsA("BasePart") then
+      
+      instance.CollisionGroup = if mode == "walking" then "Default" else "Flyers";
+      
+    end
+    
+  end
+  
+end
