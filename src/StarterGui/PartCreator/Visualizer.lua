b813417d4e7e8edc4c6e937dac27d0f8818f7021
Christian Toney <christian@makuwro.com>
2024-05-17 16:01:12 -0400

    Update Visualizer.lua

--- src/StarterGui/PartCreator/Visualizer.lua
+++ src/StarterGui/PartCreator/Visualizer.lua
@@ -3,6 +3,8 @@
 -- Written by Christian "Sudobeast" Toney
 -- This script helps the player visualize a part before they place it.
 
+local ReplicatedStorage = game:GetService("ReplicatedStorage");
+
 -- Create a preview part.
 local previewPart = Instance.new("Part");
 previewPart.CanCollide = false;
@@ -35,7 +37,21 @@ end)
 
 -- Create a part when the player presses the down button.
 mouse.Button1Down:Connect(function()
-  
-  game:GetService("ReplicatedStorage").Functions.CreatePart:InvokeServer(previewPart.CFrame)
+
+  local originalCFrame = previewPart.CFrame;
+  local partID = ReplicatedStorage.Functions.CreatePart:InvokeServer(originalCFrame);
+  game:GetService("Players").LocalPlayer.PlayerGui.History.HistoryHandler.AddToHistoryStore:Invoke({
+    label = "Created <b>Part</b>";
+    undo = function()
+      
+      ReplicatedStorage.Functions.DestroyPart:InvokeServer(partID);
+      
+    end,
+    redo = function()
+      
+      partID = ReplicatedStorage.Functions.CreatePart:InvokeServer(originalCFrame);
+      
+    end
+  })
   
 end)
