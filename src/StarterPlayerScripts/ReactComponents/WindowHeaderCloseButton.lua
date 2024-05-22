35a2b35a4e586256c785a1706de02cb5a5fc6f20
Christian Toney <christian@makuwro.com>
2024-05-21 20:35:07 -0400

    Create WindowHeaderCloseButton.lua

---
+++ src/StarterPlayerScripts/ReactComponents/WindowHeaderCloseButton.lua
@@ -0,0 +1,28 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage");
+local React = require(ReplicatedStorage.Packages.ReactLua.React);
+local ReactRoblox = require(ReplicatedStorage.Packages.ReactLua.ReactRoblox);
+
+type WindowHeaderCloseButtonProps = {
+  onClick: () -> ();
+}
+
+local function WindowHeaderCloseButton(props: WindowHeaderCloseButtonProps)
+  
+  return React.createElement("TextButton", {
+    Text = "";
+    Name = "CloseButton";
+    BackgroundColor3 = Color3.fromRGB(255, 89, 89);
+    Size = UDim2.new(0, 10, 0, 10);
+    LayoutOrder = 2;
+    [React.Event.Activated] = function()
+
+      props.onClick();
+
+    end,
+  }, {
+    React.createElement("UICorner", {CornerRadius = UDim.new(1, 0)});
+  });
+  
+end
+
+return WindowHeaderCloseButton;
