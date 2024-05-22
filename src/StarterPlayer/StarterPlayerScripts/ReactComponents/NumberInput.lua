f425155aab9467b5771b1a868a9da3abe8f8dd75
Christian Toney <christian@makuwro.com>
2024-05-22 13:52:37 -0400

    update

---
+++ src/StarterPlayer/StarterPlayerScripts/ReactComponents/NumberInput.lua
@@ -0,0 +1,62 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage");
+local React = require(ReplicatedStorage.Packages.ReactLua.React);
+local ReactRoblox = require(ReplicatedStorage.Packages.ReactLua.ReactRoblox);
+
+type NumberInputProps = {
+  label: string?;
+  value: number?;
+  onChange: (newValue: number?) -> ();
+}
+
+local function NumberInput(props: NumberInputProps)
+  
+  local TextLabel = if props.label then (
+    React.createElement("TextLabel", {
+      Name = "TextLabel";
+      BackgroundTransparency = 1;
+      Size = UDim2.new(1, 0, 0, 14);
+      TextColor3 = Color3.new(1, 1, 1);
+      TextXAlignment = Enum.TextXAlignment.Left;
+      Text = props.label;
+      FontFace = Font.fromId(11702779517);
+      TextSize = 14;
+      BorderSizePixel = 0;
+    })
+  ) else nil;
+  
+  local TextBox = React.createElement("TextBox", {
+    Name = "TextBox";
+    BackgroundTransparency = 0.8;
+    Size = UDim2.new(1, 0, 0, 30);
+    BackgroundColor3 = Color3.new(1, 1, 1);
+    TextColor3 = Color3.new(1, 1, 1);
+    TextXAlignment = Enum.TextXAlignment.Left;
+    Text = tostring(props.value);
+    FontFace = Font.fromId(11702779517);
+    TextSize = 14;
+    BorderSizePixel = 0;
+    [React.Event.FocusLost] = function(self)
+      
+      local canChangeToNumber = pcall(function() props.onChange(tonumber(self.Text)); end);
+      if not canChangeToNumber then
+        
+        self.Text = props.value;
+        
+      end
+      
+    end,
+  }, {React.createElement("UICorner", {CornerRadius = UDim.new(0, 5)})});
+  
+  return React.createElement("CanvasGroup", {
+    Name = "NumberInput";
+    BackgroundTransparency = 1;
+    Size = UDim2.new(1, 0, 0, if TextLabel then 44 else 30);
+  }, {
+    React.createElement("UIListLayout", {Name = "UIListLayout"; SortOrder = Enum.SortOrder.LayoutOrder;}), 
+    TextLabel,
+    TextBox
+  });
+  
+end
+
+return NumberInput;
--- src/StarterPlayer/StarterPlayerScripts/ReactComponents/PartOrientationModificationWindow.lua
+++ src/StarterPlayer/StarterPlayerScripts/ReactComponents/PartOrientationModificationWindow.lua
@@ -4,92 +4,132 @@ local ReactRoblox = require(ReplicatedStorage.Packages.ReactLua.ReactRoblox);
 local Window = require(script.Parent.Parent.ReactComponents.Window);
 local Dropdown = require(script.Parent.Parent.ReactComponents.Dropdown);
 local Checkbox = require(script.Parent.Parent.ReactComponents.Checkbox);
+local NumberInput = require(script.Parent.Parent.ReactComponents.NumberInput);
 
-type PartOrientationModificationWindowProps = {handle: ScreenGui};
+type PartMaterialModificationWindowProps = {handle: ScreenGui};
 
-local function PartOrientationModificationWindow(props: PartOrientationModificationWindowProps)
+local function PartMaterialModificationWindow(props: PartMaterialModificationWindowProps)
   
-  local parts: {BasePart?}, setParts = React.useState({});
+  local parts, setParts = React.useState({});
   local selectedOptionIndex, setSelectedOptionIndex = React.useState(nil);
-  local textBoxes = {};
-  for i = 1, 3 do
-    
-    table.insert(textBoxes, React.createElement("TextBox", {
-      LayoutOrder = i;
-      Name = ({"OrientationX", "OrientationY", "OrientationZ"})[i];
-      BackgroundColor3 = Color3.new(1, 1, 1);
-      BackgroundTransparency = 0.8;
-      BorderSizePixel = 0;
-      Size = UDim2.new(1 / 3, -2, 1, 0);
-      TextColor3 = Color3.new(1, 1, 1);
-      FontFace = Font.fromId(11702779517);
-      TextSize = 14;
-      Text = "";
-      [React.Event.InputEnded] = function(self)
-
-        local requestedValue = tonumber(self.Text);
-        if requestedValue then
-
-          for _, part in ipairs(parts) do
-            
-            local orientationX = if i == 1 then requestedValue else part.Orientation.X;
-            local orientationY = if i == 2 then requestedValue else part.Orientation.Y;
-            local orientationZ = if i == 3 then requestedValue else part.Orientation.Z;
-            
-            part.Orientation = Vector3.new(orientationX, orientationY, orientationZ);
-
-          end
-
-        end
-
-      end
-    }));
-    
-  end
   
+  local dropdownOptions = {};
+  for index, material in ipairs(Enum.Material:GetEnumItems()) do
+    
+    table.insert(dropdownOptions, {
+      text = material.Name;
+      onClick = function()
+        
+        setSelectedOptionIndex(index);
+        for _, part in ipairs(parts) do
+          
+          part.Material = material;
+          
+        end
+        
+      end;
+    });
+    
+  end;
+
+  local isShadowEnabled, setIsShadowEnabled = React.useState(false);
+  local transparency, setTransparency = React.useState(nil);
+  local reflectance, setReflectance = React.useState(nil);
   React.useEffect(function()
     
-    script.Parent = props.handle;
-    
     game:GetService("ReplicatedStorage").Events.SelectedPartsChanged.Event:Connect(function(selectedParts)
-
+      
+      setParts(selectedParts);
       if selectedParts[1] then
 
-        script.ArcHandles.Adornee = selectedParts[1];
-        setParts(selectedParts);
-
+        setSelectedOptionIndex(table.find(Enum.Material:GetEnumItems(), selectedParts[1].Material));
+        setIsShadowEnabled(selectedParts[1].CastShadow);
+        setReflectance(selectedParts[1].Reflectance);
+        setTransparency(selectedParts[1].Transparency);
+        
+      else
+        
+        setSelectedOptionIndex();
+        setIsShadowEnabled(false);
+        setReflectance();
+        setTransparency();
+        
       end
-
+      
     end)
     
   end, {});
   
+  
   return React.createElement(Window, {
-    name = "Orientation"; 
+    name = "Material"; 
     size = UDim2.new(0, 250, 0, 135); 
     onCloseButtonClick = function()
-      
-      script.ArcHandles.Visible = false;
+
       props.handle.Enabled = false;
 
     end
   }, {
-    React.createElement("CanvasGroup", {
-      BackgroundTransparency = 1;
-      Name = "AxesContainer";
-      Size = UDim2.new(1, 0, 0, 30);
-    }, {
-      React.createElement("UIListLayout", {
-        SortOrder = Enum.SortOrder.LayoutOrder;
-        FillDirection = Enum.FillDirection.Horizontal;
-        VerticalAlignment = Enum.VerticalAlignment.Center;
-        Padding = UDim.new(0, 2);
-      }),
-      React.createElement("UICorner", {CornerRadius = UDim.new(0, 8)}, {});
-      textBoxes
-    })
-  });
+    React.createElement(Dropdown, {selectedIndex = selectedOptionIndex; options = dropdownOptions});
+    React.createElement(Checkbox, {
+      text = "Cast shadow";
+      isChecked = isShadowEnabled;
+      onClick = function()
+        
+        if parts[1] then
+          
+          local castShadow = not parts[1].CastShadow;
+          for _, part in ipairs(parts) do
+            
+            part.CastShadow = castShadow;
+            
+          end
+          setIsShadowEnabled(castShadow);
+          
+        end
+        
+      end;
+    });
+    React.createElement(NumberInput, {
+      label = "Transparency";
+      value = transparency;
+      onChange = function(newValue)
+        
+        if parts[1] then
+          
+          newValue = if newValue > 0.8 then 0.8 else newValue;
+          
+          for _, part in ipairs(parts) do
+
+            part.Transparency = newValue;
+
+          end
+          setTransparency(parts[1].Transparency);
+          
+        end
+        
+      end;
+    });
+    React.createElement(NumberInput, {
+      label = "Reflectance";
+      reflectance = reflectance;
+      onChange = function(newValue)
+
+        if parts[1] then
+
+          for _, part in ipairs(parts) do
+
+            part.Reflectance = newValue;
+
+          end
+          setReflectance(parts[1].Reflectance);
+
+        end
+
+      end;
+    });
+  })
 
 end
 
-return PartOrientationModificationWindow;
+return PartMaterialModificationWindow;
