cf40e97b4dd41f80c42fbe7c2a8405628c873d3f
Christian Toney <christian@makuwro.com>
2024-05-23 22:53:48 -0400

    Create a surface modifier

--- src/StarterPlayer/StarterPlayerScripts/ReactComponents/PartSurfaceModificationWindow.lua
+++ src/StarterPlayer/StarterPlayerScripts/ReactComponents/PartSurfaceModificationWindow.lua
@@ -1,29 +1,63 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage");
 local React = require(ReplicatedStorage.Shared.Packages.react);
 local Window = require(script.Parent.Parent.ReactComponents.Window);
-local Checkbox = require(script.Parent.Parent.ReactComponents.Checkbox);
+local Dropdown = require(script.Parent.Parent.ReactComponents.Dropdown);
 
 type PartSurfaceModificationWindowProps = {onClose: () -> (); parts: {BasePart}; updateParts: (newProperties: any) -> ()};
 
 local function PartSurfaceModificationWindow(props: PartSurfaceModificationWindowProps)
-
-  local isAnchored, setIsAnchored = React.useState(false);
   
+  local selectedSurface, setSelectedSurface = React.useState("All");
+  local selectedSurfaceType: Enum.SurfaceType?, setSelectedSurfaceType = React.useState(nil);
+  local surfaces = {"Back", "Bottom", "Front", "Left", "Right", "Top"};
   React.useEffect(function()
     
     local events = {};
-    for i, part in ipairs(props.parts) do
-      
-      if i == 1 then
-        
-        table.insert(events, part:GetPropertyChangedSignal("Anchored"):Connect(function()
+    local firstPart = props.parts[1];
+    if firstPart then
 
-          setIsAnchored(part.Anchored);
+      if selectedSurface == "All" then
 
-        end))
-        
-      end
+        local function doSurfacesMatch()
+
+          for _, surface in ipairs(surfaces) do
+
+            if firstPart[`{surface}Surface`] ~= firstPart.BackSurface then
+
+              return false;
+
+            end;
+
+          end;
+
+          return true;
+
+        end;
       
+        for _, surface in ipairs(surfaces) do
+
+          local propertyName = `{surface}Surface`;
+          table.insert(events, firstPart:GetPropertyChangedSignal(propertyName):Connect(function()
+        
+            setSelectedSurfaceType(if doSurfacesMatch() then firstPart[propertyName] else nil);
+  
+          end));
+
+        end;
+
+        setSelectedSurfaceType(if doSurfacesMatch() then firstPart.BackSurface else nil);
+
+      else
+
+        table.insert(events, firstPart:GetPropertyChangedSignal(`{selectedSurface}Surface`):Connect(function()
+        
+          setSelectedSurfaceType(firstPart[`{selectedSurface}Surface`]);
+
+        end));
+        setSelectedSurfaceType(firstPart[`{selectedSurface}Surface`]);
+
+      end;
+
     end;
     
     return function()
@@ -36,31 +70,74 @@ local function PartSurfaceModificationWindow(props: PartSurfaceModificationWindo
       
     end
     
-  end, {props.parts});
+  end, {props.parts, selectedSurface});
+
+  local surfaceDropdownOptions = {{
+    text = "All",
+    onClick = function()
+
+      setSelectedSurface("All");
+    
+    end;
+  }};
+  for _, surface in ipairs(surfaces) do
+
+    table.insert(surfaceDropdownOptions, {
+      text = surface;
+      onClick = function()
+
+        setSelectedSurface(surface);
+
+      end;
+    })
+
+  end;
+
+  local typeDropdownOptions = {};
+  for _, surfaceType in ipairs(Enum.SurfaceType:GetEnumItems()) do
+
+    table.insert(typeDropdownOptions, {
+      text = surfaceType.Name;
+      onClick = function()
+
+        if selectedSurface == "All" then
+
+          local newProperties = {};
+          for _, surface in ipairs(surfaces) do
+
+            newProperties[`{surface}Surface`] = surfaceType;
+
+          end;
+          props.updateParts(newProperties);
+
+        else
+
+          props.updateParts({[`{selectedSurface}Surface`] = surfaceType});
+
+        end;
+
+      end;
+    });
+
+  end;
+
+  local surfacesWithAll = table.clone(surfaces);
+  table.insert(surfacesWithAll, 1, "All");
   
   return React.createElement(Window, {
     name = "Surfaces"; 
-    size = UDim2.new(0, 250, 0, 135); 
+    size = UDim2.new(0, 250, 0, 175); 
     onCloseButtonClick = props.onClose;
   }, {
-    React.createElement(Checkbox, {
-      text = "Anchored";
-      isChecked = isAnchored;
-      onClick = function()
-        
-        local possiblePart = props.parts[1];
-        if possiblePart then
-          
-          local isAnchored = not possiblePart.Anchored;
-          for _, part in ipairs(props.parts) do
-            
-            part.Anchored = isAnchored;
-            
-          end
-          
-        end
-        
-      end;
+    React.createElement(Dropdown, {
+      label = "Side";
+      selectedIndex = table.find(surfacesWithAll, selectedSurface);
+      options = surfaceDropdownOptions;
+    });
+    React.createElement(Dropdown, {
+      label = "Type";
+      selectedIndex = table.find(Enum.SurfaceType:GetEnumItems(), selectedSurfaceType);
+      options = typeDropdownOptions;
     });
   })
 
