0c6704b51142fed7c744737f916968d932274aad
Christian Toney <christian@makuwro.com>
2024-05-28 23:19:28 -0400

    Add /stages command

--- src/StarterPlayer/StarterPlayerScripts/ReactUIRoots/MainMenu.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/ReactUIRoots/MainMenu.client.lua
@@ -6,6 +6,7 @@ local StagesScreen = require(ReactComponents.StagesScreen);
 local TweenService = game:GetService("TweenService");
 local Lighting = game:GetService("Lighting");
 local StarterGui = game:GetService("StarterGui");
+local TextChatService = game:GetService("TextChatService");
 
 local player = game:GetService("Players").LocalPlayer;
 
@@ -22,20 +23,36 @@ StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, false);
 local root = ReactRoblox.createRoot(handle);
 
 local function MainMenuContainer()
+
+  React.useEffect(function()
+  
+    TweenService:Create(Lighting.Blur, TweenInfo.new(), {Size = 8}):Play();
+
+  end, {});
   
   return React.createElement(React.StrictMode, {}, {
     Screen = React.createElement(StagesScreen, {
       onStageDownloaded = function()
 
-        handle.Enabled = false;
         ReplicatedStorage.Shared.Functions.LoadCharacter:InvokeServer();
         TweenService:Create(Lighting.Blur, TweenInfo.new(), {Size = 0}):Play();
         StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, true);
+        root:unmount();
 
       end;
     });
   });
   
-end
+end;
 
+-- Handle the commands.
+TextChatService.Commands.StagesCommand.Triggered:Connect(function()
+
+  handle.Enabled = true;
+  StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, false);
+  root:render(React.createElement(MainMenuContainer));
+
+end);
+
+-- Render the GUI.
 root:render(React.createElement(MainMenuContainer));
